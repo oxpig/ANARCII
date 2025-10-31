1a393d0c87fac321b552bf10abac0d9d3aac9034
ALGW71 <GreenshieldsWatsonAL@gmail.com>
2025-10-31 15:30:34 +0000

    Keep in a verbose setting.

--- src/anarcii/pipeline/configuration.py
+++ src/anarcii/pipeline/configuration.py
@@ -20,7 +20,7 @@ def configure_cpus(ncpu: int) -> int:
         return available_cpus
 
 
-def configure_device(cpu: bool, ncpu: int) -> torch.device:
+def configure_device(cpu: bool, ncpu: int, verbose: bool) -> torch.device:
     """
     Configure computation device (CPU or GPU).
     It will default to CPU if no GPU is available or if cpu=True.
@@ -29,7 +29,10 @@ def configure_device(cpu: bool, ncpu: int) -> torch.device:
         device = torch.device("cpu")
     else:
         device = torch.device("cuda")
-    
+
+    if verbose:
+        print(f"Using device {str(device).upper()} with {ncpu} CPUs")
+
     return device
 
 
